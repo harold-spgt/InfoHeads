1dbb391eda31d82df5d71d6e2f6c51cb299e4e8b
Harry0198 <harrydrum0198@gmail.com>
2019-08-13 17:09:55 +0100

    Code cleanup

--- InfoHeads/src/main/java/me/harry0198/infoheads/inventorys/HeadStacks.java
+++ InfoHeads/src/main/java/me/harry0198/infoheads/inventorys/HeadStacks.java
@@ -7,7 +7,6 @@ import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.SkullMeta;
 
-import java.util.Arrays;
 import java.util.List;
 
 public class HeadStacks {
@@ -30,8 +29,6 @@ public class HeadStacks {
 
         return list;
     }
-
-
 
     void giveItems(Player player) {
 
