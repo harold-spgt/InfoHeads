4e3aac3310e7eb53a19a4c550f713d1f61d53323
Harry0198 <harrydrum0198@gmail.com>
2020-03-28 21:49:12 +0000

    Recode

--- InfoHeads/src/main/java/me/harry0198/infoheads/utils/Constants.java
+++
@@ -1,7 +0,0 @@
-package me.harry0198.infoheads.utils;
-
-public final class Constants {
-
-    public static final String BASE_PERM = "infoheads.";
-    public static final String ADMIN_PERM = "infoheads.admin";
-}
